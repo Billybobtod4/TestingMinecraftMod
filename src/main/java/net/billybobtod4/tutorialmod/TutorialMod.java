009df9e94fd456125f8d0c7251f7360e39919900
Billybobtod4 <billypadie@gmail.com>
2024-06-20 00:25:33 -0400

    Added 2 Items (Does nothing yet)

--- src/main/java/net/billybobtod4/tutorialmod/TutorialMod.java
+++ src/main/java/net/billybobtod4/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.billybobtod4.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.billybobtod4.tutorialmod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,6 +20,8 @@ public class TutorialMod {
     //test commit message
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
